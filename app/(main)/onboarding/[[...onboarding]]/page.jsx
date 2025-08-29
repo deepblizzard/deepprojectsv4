b02aabe53e9ae8ef298a265e96137d368315d799
antiSymmetric <pixeldrift.adv@proton.me>
2025-08-30 01:26:39 +0530

    *

--- app/(main)/onboarding/[[...onboarding]]/page.jsx
+++ app/(main)/onboarding/[[...onboarding]]/page.jsx
@@ -1,26 +1,15 @@
-"use client";
+'use client';
 
-import { OrganizationList, useOrganization } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { OrganizationList } from '@clerk/nextjs';
 
 export default function Onboarding() {
-  const { organization } = useOrganization();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (organization) {
-      router.push(`/organization/${organization.slug}`);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [organization]);
-
   return (
     <div className="flex justify-center items-center pt-14">
       <OrganizationList
         hidePersonal
-        afterCreateOrganizationUrl="/organization/:slug"
-        afterSelectOrganizationUrl="/organization/:slug"
+        // Dynamic redirect using function callback
+        afterCreateOrganizationUrl={(org) => `/organization/${org.slug}`}
+        afterSelectOrganizationUrl={(org) => `/organization/${org.slug}`}
       />
     </div>
   );
